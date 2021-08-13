8e5a16b14c36697f466f25f9c2946fcf50dce53e
Nevin Trian <55471127+nevintrian@users.noreply.github.com>
2021-08-13 17:07:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/catatanharian/DailyNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.catatanharian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_note);
+    }
+}
