03e479d962eb002e340825de19d5ea50ad2d5dc0
Nevin Trian <55471127+nevintrian@users.noreply.github.com>
2021-08-17 19:23:17 +0700

    internal storage

--- app/src/main/java/com/example/catatanharian/LoginActivity.java
+++ app/src/main/java/com/example/catatanharian/LoginActivity.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +32,39 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void actionLogin(View view) {
-        if(username.getText().toString().isEmpty()){
+
+        if (username.getText().toString().isEmpty()) {
             Toast.makeText(this, "Username belum diisi", Toast.LENGTH_SHORT).show();
-        }else if(password.getText().toString().isEmpty()){
+        } else if (password.getText().toString().isEmpty()) {
             Toast.makeText(this, "Password belum diisi", Toast.LENGTH_SHORT).show();
-        }else if(username.getText().toString().equals("nevin") && password.getText().toString().equals("nevin123")){
-            startActivity(new Intent(this,MainActivity.class));
-            Toast.makeText(this, "Berhasil login", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "username atau password salah", Toast.LENGTH_SHORT).show();
+        } else {
+            File sdcard = getFilesDir();
+            File file = new File(sdcard, username.getText().toString());
+            if (file.exists()) {
+                StringBuilder text = new StringBuilder();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line = br.readLine();
+                    while (line != null) {
+                        text.append(line);
+                        line = br.readLine();
+                    }
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println("Error " + e.getMessage());
+                }
+                String data = text.toString();
+                String[] dataUser = data.split(";");
+
+                if (dataUser[1].equals(password.getText().toString())) {
+                    startActivity(new Intent(this, MainActivity.class));
+                    Toast.makeText(this, "Berhasil login", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Password tidak sesuai", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Username tidak ditemukan", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
--- app/src/main/java/com/example/catatanharian/SignUpActivity.java
+++ app/src/main/java/com/example/catatanharian/SignUpActivity.java
@@ -5,17 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText username, password, ulangiPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         setTitle("Registrasi");
+        username = findViewById(R.id.edt_username);
+        password = findViewById(R.id.edt_password);
+        ulangiPassword = findViewById(R.id.edt_ulangi_password);
     }
 
     public void actionLoginDisini(View view) {
         startActivity(new Intent(this,LoginActivity.class));
+    }
+
+    public void actionDaftar(View view) {
+        if(password.getText().toString().equals(ulangiPassword.getText().toString())){
+            String isiFile =
+                    username.getText().toString() + ";" +
+                    password.getText().toString();
+
+            File file = new File(getFilesDir(), username.getText().toString());
+            FileOutputStream outputStream = null;
+            try{
+                file.createNewFile();
+                outputStream = new FileOutputStream(file, false);
+                outputStream.write(isiFile.getBytes());
+                outputStream.flush();
+                outputStream.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            Toast.makeText(this, "Berhasil daftar, silahkan login", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this,LoginActivity.class));
+        }else{
+            Toast.makeText(this, "Password yang diisikan tidak sama", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
